22485073668561f9e276021ff780e54800f381ed
thalita <e107ti@etec.sp.gov.br>
2023-08-18 22:45:52 -0300

    exercicio resolvido

--- Program.cs
+++ Program.cs
@@ -1,2 +1,65 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Write("Dó, ");
+Console.Beep(1320, 200);
+Console.Write("Ré, ");
+Console.Beep(1485, 200);
+Console.Write("Mi, ");
+Console.Beep(1650; 200);
+Console.Write("Fá, ");
+Console.Beep(1759, 200);
+Thread.Sleep(500);
+Console.Write("Fá, ");
+Console.Beep(1759, 200);
+Console.Write("Fá, ");
+Console.Beep(1759, 200);
+Thread.Sleep(1000);
+
+Console.WriteLine("");
+
+Console.Write("Dó, ");
+Console.Beep(1320, 200);
+Console.Write("Ré, ");
+Console.Beep(1485, 200);
+Console.Write("Dó, ");
+Console.Beep(1320, 200);
+Console.Write("Ré, ");
+Console.Beep(1485, 200);
+Thread.Sleep(500);
+Console.Write("Ré, ");
+Console.Beep(1485, 200);
+Console.Write("Ré, ");
+Console.Beep(1485, 200);
+Thread.Sleep(1000);
+
+Console.WriteLine("");
+
+Console.Write("Dó, ");
+Console.Beep(1320, 200);
+Console.Write("Sol, ");
+Console.Beep(1980, 200);
+Console.Write("Fá, ");
+Console.Beep(1759, 200);
+Console.Write("Mi, ");
+Console.Beep(1650, 200);
+Thread.Sleep(500);
+Console.Write("Mi, ");
+Console.Beep(1650, 200);
+Console.Write("Mi, ");
+Console.Beep(1650, 200);
+Thread.Sleep(1000);
+
+Console.WriteLine("");
+
+Console.WriteLine("Dó, ");
+Console.Beep(1320, 200);
+Console.Write("Ré, ");
+Console.Beep(1485, 200);
+Console.Write("Mi, ");
+Console.Beep(1650, 200);
+Console.Write("Fá, ");
+Console.Beep(1759, 200);
+Thread.Sleep(500);
+Console.Write("Fá, ");
+Console.Beep(1759, 200);
+Console.Write("Fá, ");
+Console.Beep(1759, 200);
